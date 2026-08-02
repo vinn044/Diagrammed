fd415c9f99f2ec689ad49f1bf5fc9a00ea7afca9
Vince Kalinowski <vincek1800@gmail.com>
2026-08-02 01:50:32 -0500

    basic auth running, login/signup page created

--- testApp/urls.py
+++ testApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
